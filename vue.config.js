eb4ef40343c48647a40fb87be86528442adf7875
“yu.pan <yu.pan@hzjo.com>
2020-03-30 18:21:53 +0800

    优化环境变量配置

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ const path = require('path');//引入path模块
 function resolve(dir) {
     return path.join(__dirname, dir)//path.join(__dirname)设置绝对路径
 }
-
+console.log(`NODE_ENV:${process.env.NODE_ENV}`,`VUE_APP_API_ENV:${process.env.VUE_APP_API_ENV}`,`API_ENV:${process.env.API_ENV}`)
 module.exports = {
-    lintOnSave:'error',
+    lintOnSave: 'error',
     chainWebpack: (config) => {
         config.resolve.alias
             .set('@', resolve('./src'))
@@ -22,10 +22,19 @@ module.exports = {
             .options({
                 symbolId: 'icon-[name]'
             })
+
+        // vue-cli3 中配置了类似功能且实现效果对比 cross-env 更优
+        // // 配置 cross-env 全局环境变量
+        // config
+        //     .plugin('define')
+        //     .tap(args => {
+        //         Object.assign(args[0]['process.env'],{API_ENV:`"${process.env.API_ENV}"`});
+        //         return args
+        //     })
     },
     css: {
-        loaderOptions:{
-            scss:{
+        loaderOptions: {
+            scss: {
                 prependData: `@import "~@/styles/index.scss";`
             }
         }

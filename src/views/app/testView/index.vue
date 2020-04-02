53741ebf9dfa604c0bc4cf35e485ed2403cf4607
“yu.pan <yu.pan@hzjo.com>
2020-04-02 18:08:24 +0800

    修改table组件选择双向绑定逻辑

--- src/views/app/testView/index.vue
+++ src/views/app/testView/index.vue
@@ -1,41 +1,12 @@
 <template>
     <div>
-        <sas-table :data="tableData" :columnList="columnList" v-model="select" ref="testTable" :reserveSelection="true"
-                   row-key="id" @select="handlerSelect"/>
-        <el-button @click="handlerFirst">first</el-button>
-        <el-button @click="handlerNext">next</el-button>
-        <el-button @click="handlerTest">test</el-button>
-        <el-button @click="handlerTes">tes</el-button>
     </div>
 </template>
 
 <script>
     export default {
         name: "index",
-        data() {
-            return {
-                select: [],
-                tableData: [],
-                columnList: [{type: 'selection'}, {label: 'id', key: 'id'}]
-            }
-        },
-        methods: {
-            handlerSelect(select, row) {
-                console.log(select, row)
-            },
-            handlerTes() {
-                this.select = []
-            },
-            handlerTest() {
-                this.select = [{id: '456'}]
-            },
-            handlerFirst() {
-                this.tableData = [{id: '123'}, {id: '223'}]
-            },
-            handlerNext() {
-                this.tableData = [{id: '456'}]
-            }
-        }
+
     }
 </script>
 
